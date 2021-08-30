590768d9d3cc52257d8e94208c555d03164ab0f8
rDk107c <radoslaw.kowalik90@gmail.com>
2021-08-30 20:11:59 +0200

    BufferedReader

--- src/main/java/pl/sda/buffering/Main.java
+++ src/main/java/pl/sda/buffering/Main.java
@@ -5,10 +5,17 @@ import java.io.*;
 public class Main {
 
     public static void main(String[] args) {
-        try(BufferedReader in = new BufferedReader(new FileReader("user.txt"))) {
-            String line;
-            while ((line = in.readLine()) != null) {
-                System.out.println(line);
+        try(BufferedReader in = new BufferedReader(new FileReader("user.txt"));
+            BufferedWriter out = new BufferedWriter(new FileWriter("user_copy.txt"))) {
+            String line = in.readLine();
+            String nextline;
+            while (line != null) {
+                nextline = in.readLine();
+                out.write(line);
+                if (nextline != null){
+                    out.newLine();
+                }
+                line = nextline;
             }
         } catch (IOException e) {
             e.printStackTrace();
