ad10d5b9bb97c8d05e774f91b6b8acf319c8d299
Julia Yu <jjjuliayyyu@gmail.com>
2021-08-30 19:24:56 +0200

    "metoda append"

--- src/main/java/pl/sda/characterstream/Main.java
+++ src/main/java/pl/sda/characterstream/Main.java
@@ -1,15 +1,19 @@
 package pl.sda.characterstream;
 
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class Main {
 
     public static void main(String[] args) {
-        try(FileReader reader = new FileReader("user.txt")) {
+        try(FileReader reader = new FileReader("user.txt");
+            FileWriter writer = new FileWriter("user_copy.txt")) {
             int dataByteAsInt;
             while ((dataByteAsInt = reader.read()) != -1) {
                 char character = (char) dataByteAsInt;
+
+                writer.append(character);
                 System.out.print(character);
             }
         } catch (IOException e) {
