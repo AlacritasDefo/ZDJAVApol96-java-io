fec30f730e7e12bc458a3e77f147bf24dadbfa61
Jaroslaw Lada <lada.jaroslaw@gmail.com>
2021-08-30 19:12:50 +0200

    FileOutputStream exercise

--- src/main/java/pl/sda/bytestream/Main.java
+++ src/main/java/pl/sda/bytestream/Main.java
@@ -1,15 +1,19 @@
 package pl.sda.bytestream;
 
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class Main {
 
     public static void main(String[] args) {
-        try(FileInputStream in = new FileInputStream("user.txt")) {
+        try(FileInputStream in = new FileInputStream("user.txt");
+            FileOutputStream out = new FileOutputStream("user_copy.txt")) {
+
             int dataByteAsInt;
             while ((dataByteAsInt = in.read()) != -1) {
                 char character = (char) dataByteAsInt;
+                out.write(character);
                 System.out.print(character);
             }
         } catch (IOException e) {
