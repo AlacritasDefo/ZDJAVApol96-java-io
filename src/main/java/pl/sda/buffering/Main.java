496289dd186caf52785400850dc16e9ba5537c94
AlacritasDefo <arkadiuszprzychocki@gmail.com>
2022-01-12 17:12:56 +0100

    Java IO

--- src/main/java/pl/sda/buffering/Main.java
+++ src/main/java/pl/sda/buffering/Main.java
@@ -5,14 +5,14 @@ import java.io.*;
 public class Main {
 
     public static void main(String[] args) {
-        try(BufferedReader in = new BufferedReader(new FileReader("user.txt"));
-            BufferedWriter out = new BufferedWriter(new FileWriter("user_copy.txt"))) {
+        try (BufferedReader in = new BufferedReader(new FileReader("user.txt"));
+             BufferedWriter out = new BufferedWriter(new FileWriter("user_copy.txt"))) {
             String line = in.readLine();
             String nextline;
             while (line != null) {
                 nextline = in.readLine();
                 out.write(line);
-                if (nextline != null){
+                if (nextline != null) {
                     out.newLine();
                 }
                 line = nextline;
